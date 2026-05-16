7a51fd206bd63cf2936c850a3366f7dfc3e92679
Luisa Salamanca <luisasalamanca.fc@academia.umb.edu.co>
2026-05-16 11:53:31 -0500

    fix: usar VITE_API_URL en produccion

--- parqueadero-frontend/src/api/client.js
+++ parqueadero-frontend/src/api/client.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL:'http://localhost:3000/api' })
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
+})
 
 // Adjuntar token JWT en cada petición
 api.interceptors.request.use(cfg => {
